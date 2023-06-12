13933547f7eeea8ea7e804767ef4ff240dc1bf42
lucas-hack <130858096+lucas-hack@users.noreply.github.com>
2023-06-12 15:37:08 -0300

    um pouco de estilização
    estilizando os inputs e spans, sinto que as fontes estão no tamanho errado, ou meu monitor que é meio errado

--- src/pages/Register/index.js
+++ src/pages/Register/index.js
@@ -1,46 +1,58 @@
-import "./estilo.css"
+import "./estilo.css";
 
 function Register() {
   return (
     <div className="container">
-
       <div>
         <h1 className="titulo">Welcome,</h1>
         <h2 className="subTitulo">Please, register to continue</h2>
 
-        <form>
-          <span>first name</span>
-          <input type="text" />
+        <form className="form">
+          <div className="inputGroup">
+            <span>first name</span>
+            <input type="text" placeholder="Your first name" />
+          </div>
 
-          <span>last name</span>
-          <input type="text" />
+          <div className="inputGroup">
+            <span>last name</span>
+            <input type="text" placeholder="Your last name" />
+          </div>
 
-          <span>birth date</span>
-          <input type="text" />
+          <div className="inputGroup">
+            <span>birth date</span>
+            <input type="text" placeholder="MM/DD/YYYY" />
+          </div>
 
-          <span>Country</span>
-          <input type="text" />
+          <div className="inputGroup">
+            <span>Country</span>
+            <input type="text" placeholder="Your Country" />
+          </div>
 
-          <span>City</span>
-          <input type="text" />
+          <div className="inputGroup">
+            <span>City</span>
+            <input type="text" placeholder="Your City" />
+          </div>
 
-          <span>e-mail</span>
-          <input type="text" />
+          <div className="inputGroup">
+            <span>e-mail</span>
+            <input type="text" placeholder="A valid e-mail here" />
+          </div>
 
-          <span>password</span>
-          <input type="text" />
+          <div className="inputGroup">
+            <span>password</span>
+            <input type="text" placeholder="Your password" />
+          </div>
 
-          <span>password</span>
-          <input type="text" />
+          <div className="inputGroup">
+            <span>password</span>
+            <input type="text" placeholder="Comfirm your password" />
+          </div>
 
           <button>Register Now</button>
-
         </form>
       </div>
 
-      <div className="foto">
-      </div>
-
+      <div className="foto"></div>
     </div>
   );
 }
