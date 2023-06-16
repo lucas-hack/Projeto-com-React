be417ec2cb74fc15a48a829285d8c46faa92df3d
lucas-hack <130858096+lucas-hack@users.noreply.github.com>
2023-06-16 01:56:42 -0300

    redirecionando
    fazendo o redirecionamento quando é cadastrado, e depois colocar os alertas personalizados

--- src/contexts/auth.js
+++ src/contexts/auth.js
@@ -2,11 +2,15 @@ import { ùseState, createContext, useState } from "react"
 import { auth, db } from "../services/firebaseConnection"
 import { createUserWithEmailAndPassword } from "firebase/auth"
 import { doc, getDoc, setDoc } from "firebase/firestore"
+import {toast} from "react-toastify"
+import { useNavigate } from "react-router-dom"
 
 export const AuthContext = createContext({})
 
 function AuthProvider({ children }) {
     const [user, setUser] = useState(null)
+
+    const navigate = useNavigate()
 
     // função para logar o usuário
     function singIn(email, password) {
@@ -16,7 +20,7 @@ function AuthProvider({ children }) {
     }
 
     // função para criar um novo usuário
-    async function newUser(firstNameFormat,lastNameFormat ,countryFormat , cityFormat, email, password) {
+    async function newUser(firstNameFormat, lastNameFormat, countryFormat, cityFormat, email, password) {
 
         await createUserWithEmailAndPassword(auth, email, password)
             .then(async (value) => {
@@ -29,13 +33,29 @@ function AuthProvider({ children }) {
                     city: cityFormat,
                 })
                     .then(() => {
-                        alert("cadastrado com sucesso")
+
+                        let data = {
+                            uid: uid,
+                            nome: firstNameFormat,
+                            email: value.user.email,
+                            pais: countryFormat,
+                            cidade: cityFormat
+                        }
+
+                        setUser(data)
+                        storageUser(data)
+                        toast.success("Cadastro com sucesso")
+                        navigate("/dashboard")
                     })
             })
             .catch((error) => {
                 console.log(error)
             })
 
+    }
+
+    function storageUser(data) {
+        localStorage.setItem("#local", JSON.stringify(data))
     }
 
     return (
