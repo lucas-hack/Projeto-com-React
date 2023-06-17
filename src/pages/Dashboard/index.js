6775b7c31fd8f6133f3f5ef595cc4c92c1d75af6
lucas-hack <130858096+lucas-hack@users.noreply.github.com>
2023-06-17 17:56:21 -0300

    mudando de cor
    mudando o input de cor quando der erro

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,7 +1,26 @@
+
+
 export default function Dashboard() {
+
     return (
-        <div>
-            <h1>dashboard</h1>
-        </div>
+        <>
+            <header>
+                <div>
+                    <h1>Weekly Planner </h1>
+                    <p>Use this planner to organize your daily issues.</p>
+                </div>
+
+                <div>
+                    <h2>api com horário</h2>
+                </div>
+
+                <div>api do tempo</div>
+
+                <div>
+                    <a href="https://compass.uol/en/home/"><img src="" /></a>
+                    <button><img src="" /></button>
+                </div>
+            </header>
+        </>
     )
 }
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -16,6 +16,7 @@ function Home() {
 
     const { singIn } = useContext(AuthContext)
 
+    const [mensagemErro, setMensagemErro] = useState()
 
     async function handleSingIn(e) {
         e.preventDefault()
@@ -66,10 +67,13 @@ function Home() {
                     </div>
 
 
-                    <div className="inputGroup">
+                    <div className="inputGroup erro">
                         <input type="password" placeholder="password" value={password} onChange={(e) => setPassword(e.target.value)} />
                         <img src={IconP} style={{ position: 'absolute', top: 90, left: 393, marginLeft: imagePosition }} />
                     </div>
+
+                    <small>Wow, invalid username or password.
+                        Please, try again!</small>
 
                     <button type="submit" className="botao">Register Now</button>
                 </form>
