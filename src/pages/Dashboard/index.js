f42668d48f38564716464cc9266890ccf7203123
lucas-hack <130858096+lucas-hack@users.noreply.github.com>
2023-06-18 19:50:54 -0300

    cabeçalho do dashboard
    ainda precisa arrumar os inputs

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,20 +1,37 @@
-import {useContext} from "react"
-import {AuthContext} from "../../contexts/auth"
+import { useContext } from "react"
+import { AuthContext } from "../../contexts/auth"
+import logoCompass from "../../images/logo_pequeno_compass.png"
+import "./dashboard.css"
+import LogoutIcon from "../../images/icons/icon_logout.svg"
 
 export default function Dashboard() {
 
-    const {logout} = useContext(AuthContext)
+    const { logout } = useContext(AuthContext)
 
-    async function handleLogout(){
+    async function handleLogout() {
         await logout()
     }
 
     return (
-        <div>
-            <h1>dashboard</h1>
-<p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Esse deleniti reiciendis et nobis dolor libero quas accusantium asperiores ut animi mollitia, facere voluptatum possimus magni, quos impedit aliquam reprehenderit enim.</p>
+        <div className="bodyFull">
+            <header className="cabecalho">
 
-<a onClick={handleLogout}>sair da conta</a>
+                <div className="blockPreto">
+                    <h2 className="blockTitulo">Weekly Planner</h2>
+                    <p className="blockParag">Use this planner to organize your daily issues.</p>
+                </div>
+
+                <div>api do tempo</div>
+
+                <div>api do clima</div>
+
+                <div className="blockLinks">
+                    <img className="positionLogo" src={logoCompass} />
+                    <a className="positionLogout" onClick={handleLogout}><img src={LogoutIcon} /></a>
+                </div>
+
+            </header>
+
 
         </div>
     )
